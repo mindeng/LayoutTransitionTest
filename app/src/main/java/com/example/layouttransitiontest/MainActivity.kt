77f25b3eb1f45407e2f1aa4849aa8572b526891d
dengmin <minotes.net@gmail.com>
2020-12-07 18:43:49 +0800

    bubble animation

--- app/src/main/java/com/example/layouttransitiontest/MainActivity.kt
+++ app/src/main/java/com/example/layouttransitiontest/MainActivity.kt
@@ -1,40 +1,83 @@
 package com.example.layouttransitiontest
 
+import android.animation.LayoutTransition
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-
   private lateinit var mBtnDel: View
   private lateinit var mBtnAdd: View
   private lateinit var mContainer: ViewGroup
 
+  @SuppressLint("SetTextI18n")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
-    setContentView(R.layout.activity_main)
-    mContainer = findViewById<View>(R.id.ll_container) as ViewGroup
+    setContentView(R.layout.layout2)
+    mContainer = findViewById<View>(R.id.bubbleArea) as ViewGroup
+
+    findViewById<ViewGroup>(android.R.id.content).layoutTransition = createLayoutTransition()
+    findViewById<ViewGroup>(R.id.layoutContainer).layoutTransition = createLayoutTransition()
+
+//    findViewById<ViewGroup>(R.id.areaComments).layoutTransition = createLayoutTransition()
+//    findViewById<ViewGroup>(R.id.areaGifts).layoutTransition = createLayoutTransition()
+
+    mContainer.layoutTransition = createBubbleTransition()
+
     mBtnAdd = findViewById<View>(R.id.btnadd)
     mBtnAdd.setOnClickListener {
-      val button = Button(this@MainActivity)
+      val button = TextView(this@MainActivity)
       button.setPadding(20, 20, 20, 20)
-      button.text = "tempBtn"
-      mContainer.addView(button, 2)
+      button.text = "❤️"
+      button.textSize = 60f
+
+      button.setOnClickListener {
+
+      }
+
+      val params = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+          ViewGroup.LayoutParams.WRAP_CONTENT)
+      mContainer.addView(button, mContainer.childCount, params)
+
     }
 
     mBtnDel = findViewById<View>(R.id.btndel)
     mBtnDel.setOnClickListener {
-      val count: Int = mContainer.getChildCount()
-      if (count >= 3) {
-        mContainer.removeViewAt(2)
+      val count: Int = mContainer.childCount
+      if (count >= 1) {
+        mContainer.removeViewAt(count - 1)
       }
     }
+  }
+
+  private fun createBubbleTransition(): LayoutTransition {
+    val layoutTransition = LayoutTransition()
+    val addAnimator = ObjectAnimator.ofFloat(null, View.TRANSLATION_X, 0f, 400f, 0f)
+        .setDuration(1500)
+    layoutTransition.setAnimator(LayoutTransition.DISAPPEARING, addAnimator)
+
+//    val removeAnimator = ObjectAnimator.ofFloat(null, View.SCALE_X, 0.5f, 1f)
+//        .setDuration(1500)
+
+    val removeAnimator = ObjectAnimator.ofFloat(null, View.TRANSLATION_X, 400f, 0f, 0f)
+        .setDuration(1500)
+    layoutTransition.setAnimator(LayoutTransition.APPEARING, removeAnimator)
+    return layoutTransition
+  }
+
+  private fun createLayoutTransition(): LayoutTransition {
+    val layoutTransition = LayoutTransition()
+    layoutTransition.enableTransitionType(LayoutTransition.CHANGING)
+    return layoutTransition
   }
 
   override fun onCreateOptionsMenu(menu: Menu): Boolean {
